be5d677ad5c4ec27a642f3ea93d55c548ae45978
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-25 16:15:51 +0100

    functions imported from redux

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { todosReducer } from './reducers/reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const combinedReducer = combineReducers({
+  todos: todosReducer
+})
+
+const store = createStore(
+  combinedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store ={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 
